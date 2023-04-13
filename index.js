4c196c2e0b25a24229688c07bb6cad218afda27a
Abdela umer <adheremo65@gmail.com>
2023-04-12 20:31:19 -0400

    advanced first class function lab is done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (x=['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return x.slice(0,2);
+}
+const returnLastTwoDrivers = function (x=['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return x.slice(2,5);
+}
+
+  const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+  function createFareMultiplier(Y){
+    let X = 5;
+    return function (Y){
+        return Y*X;
+    }
+  }
+
+  function fareDoubler(n){
+    let m = 2*n
+    return m; 
+  }
+
+  function fareTripler(u){
+    let z = 3*u;
+    return z; 
+  }
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+function slicer(array){
+  let sliced = array.slice(0,2)
+  return sliced;
+}
+
+function selectDifferentDrivers(array,cb){
+  return cb(array);
+}
